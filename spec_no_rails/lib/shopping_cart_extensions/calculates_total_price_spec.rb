82b9fdb14e3a3bd562b443c5cc797f2435c220ef
coreyhaines <coreyhaines@gmail.com>
2011-08-24 22:59:53 +0200

    Remove half-off requirement

--- spec_no_rails/lib/shopping_cart_extensions/calculates_total_price_spec.rb
+++ spec_no_rails/lib/shopping_cart_extensions/calculates_total_price_spec.rb
@@ -8,7 +8,7 @@ end
 describe "Calculating Total Price" do
   it "returns 50% of the total prices" do
     cart = DummyCartWithProducts.new
-    products = [stub(price: 20), stub(price: 10)]
+    products = [stub(price: 5), stub(price: 10)]
     cart.stub(:products) { products }
     cart.total_price.should == 15
   end
