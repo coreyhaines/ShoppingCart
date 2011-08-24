c3d7e9735b145fa24b3d534e2bfcc41bbef60e24
coreyhaines <coreyhaines@gmail.com>
2011-08-24 20:22:01 +0200

    Remove half off requirement

--- lib/shopping_cart_extensions/calculates_total_price.rb
+++ lib/shopping_cart_extensions/calculates_total_price.rb
@@ -1,7 +1,7 @@
 module ShoppingCartExtensions
   module CalculatesTotalPrice
     def total_price
-      products.map(&:price).inject(&:+) * 0.5
+      products.map(&:price).inject(&:+)
     end
   end
 end
