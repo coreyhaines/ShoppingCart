d76d18564365fbc5f206898de0f526d77c8c85fd
coreyhaines <coreyhaines@gmail.com>
2011-09-13 09:16:16 -0500

    Redirect to shopping cart that added product

--- app/controllers/shopping_cart_products_controller.rb
+++ app/controllers/shopping_cart_products_controller.rb
@@ -1,6 +1,6 @@
 class ShoppingCartProductsController < ApplicationController
   def create
-    ShoppingCartProduct.create params[:shopping_cart_product]
-    redirect_to shopping_cart_url(:id => "1")
+    product = ShoppingCartProduct.create params[:shopping_cart_product]
+    redirect_to shopping_cart_url(id: product.shopping_cart_id)
   end
 end
