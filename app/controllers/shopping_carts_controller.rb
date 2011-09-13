083026d8c8e1c08b2b1718d4ec32d850c89633a8
coreyhaines <coreyhaines@gmail.com>
2011-09-13 09:10:40 -0500

    Minor updates for formatting

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -1,7 +1,11 @@
 class ShoppingCartsController < ApplicationController
+  def index
+    @shopping_carts = ShoppingCart.all
+  end
+
   def show
     @shopping_cart = ShoppingCart.find params[:id]
     @available_products = Product.all
-    @total_price = @shopping_cart.products.map(&:price).inject(&:+)
+    @total_price = @shopping_cart.products.map(&:price).inject(0, &:+)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ShoppingCart.destroy_all
+ShoppingCart.create description: "Corey's Cart"
+
+Product.destroy_all
+Product.create description: "Super cool foamy soap", price: 5
+Product.create description: "Boring old soap", price: 1
+Product.create description: "Fun time bubbles", price: 10
+
