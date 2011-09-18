09a7558b23cbcc7257562bf7805585e9a8850df5
Gary Bernhardt <gary.bernhardt@gmail.com>
2011-09-17 23:12:53 -0700

    Replace dummy class with stub extend

--- spec_no_rails/lib/shopping_cart_extensions/calculates_total_price_spec.rb
+++ spec_no_rails/lib/shopping_cart_extensions/calculates_total_price_spec.rb
@@ -1,13 +1,8 @@
 require 'shopping_cart_extensions/calculates_total_price'
 
-
-class DummyCartWithProducts
-  include ShoppingCartExtensions::CalculatesTotalPrice
-end
-
 describe "Calculating Total Price" do
   it "returns the total price of the products" do
-    cart = DummyCartWithProducts.new
+    cart = stub.extend(ShoppingCartExtensions::CalculatesTotalPrice)
     products = [stub(price: 5), stub(price: 10)]
     cart.stub(:products) { products }
     cart.total_price.should == 15
